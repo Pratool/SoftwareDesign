fb16872135960cae4a6ae94c0caf81f01e302ef3
Pratool <pratool.gadtaula@students.olin.edu>
2014-03-02 22:46:12 -0500

    More homework 5

--- hw5/data_mining.py
+++ hw5/data_mining.py
@@ -15,6 +15,7 @@ You have to trace a path between two different Wikipedia articles.
 """
 
 from pattern.web import *
+import math
 import pickle
 
 w = Wikipedia()
@@ -51,21 +52,107 @@ def reverse_articles(article1, article2_title):
     
     return 0
 
-def get_next_path(article, target_article):
+counter = 0
+
+def get_path(article, target_article, path):
     """
     Text analysis part. This might be better if further broken down.
+    
+    **NOTE: Already being further broken down**
     
     TODO:
     Find the next article along the path of between 2 articles by analyzing the
     similarity between the a hyperlinked article from one page to that page and
     the target page.
     """
+    global w
+    global counter
+    if counter < 5:
+        print counter
+        if reverse_articles(article, target_article.title) == 1:
+            return [article.title, target_article.title]
+        else:
+            counter = counter + 1
+            if counter == 1:
+                path.append(article.links[6]);
+                get_path(w.search(article.links[6]), target_article, path)
+            if article.links[6] == path[len(path)-1]:
+                path.append(article.links[7])
+                get_path(w.search(article.links[7]), target_article, path)
+            else:
+                path.append(article.links[6]);
+                get_path(w.search(article.links[6]), target_article, path)
     
+    return path
+#    return 'Too long'
+#    return 'Could not find direct link'
 
+def find_next_article(article, target_article):
+    global w;
+    text_init = article.links
+    text_targ = get_link_freq(target_article.links)
+    all_links = []
+    
+    for link in text_init:
+        all_links.append(get_link_freq(w.search(link).links))
+        print link
+    
+    for i in len(range(all_links)):
+        print i
+        if links_analysis(text_targ, all_links[i]) > links_analysis(text_targ, all_links[i+1]):
+            article_name = text_init[i]
+    
+    return article_name;
+        
+def links_analysis(target_links, links2):
+    global w
+    
+    num_same = 0
+    
+#    target_links = get_link_freq(target_links)
+#    links2 = get_link_freq(links2)
+    
+    for title in target_links:
+        if title in links2:
+            num_same = num_same + 1;
+    
+    return num_same / float(len(target_links))
 
-ireland_article = w.search('Ireland')
-uk_article = w.search('United Kingdom')
-nepal_article = w.search('Nepal')
+def get_link_freq(links):
+    hyperlinks = {}
+    
+    for the_links in links:
+        if the_links in hyperlinks:
+            hyperlinks[the_links] = hyperlinks[the_links] + 1
+        else:
+            hyperlinks[the_links] = 1
+    
+    return hyperlinks
+    
+def shorter_dict(dict1, dict2):
+    if (len(dict1) <= len(dict2)):
+        return [dict1, dict2]
+    return [dict2, dict1]
 
-print reverse_articles(ireland_article, uk_article.title)   # Returns 1 because they do link to each other
+#ireland_article = w.search('Ireland')
+#uk_article = w.search('United Kingdom')
+#nepal_article = w.search('Nepal')
+#aegean_article = w.search('Aegean Sea')
+#fifa_article = w.search('FIFA World Cup')
+
+tudor = w.search('Tudor conquest of Ireland')
+plant = w.search('Plantation of Ulster')
+
+#print links_analysis(ireland_article.links, uk_article.links)
+#print links_analysis(ireland_article.links, nepal_article.links)
+#print links_analysis(ireland_article.links, ireland_article.links)
+
+print find_next_article(tudor, plant)
+
+#print reverse_articles(ireland_article, uk_article.title)   # Returns 1 because they do link to each other
 #print reverse_articles(ireland_article, nepal_article.title)   # Returns 0 because they don't link to each other
+
+#print get_path(ireland_article, nepal_article, [])
+
+#print get_next_path(ireland_article, uk_article)
+#print get_next_path(ireland_article, nepal_article)
